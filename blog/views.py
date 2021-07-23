a8b9749d36acb8bb899d72a7f9bec9878c786ad2
hirakawaaa <hirakawac21@gmail.com>
2021-07-23 20:16:26 +0900

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
-
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 
@@ -10,4 +10,6 @@ def post_list(request):
     return render(request, 'blog/post_list.html', {'posts':posts})
 
 
-    
+def post_detail(request,pk):
+    post=get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post':post})
